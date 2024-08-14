225db7f2b4a6fc53cf6c6d1f5bfb53085502f72c
Anna <m.a.s.1126@yandex.ru>
2024-08-14 23:43:38 +0300

    ООП Объекты и классы #2. Линия

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,13 +2,17 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        Point point1 = new Point(1,3);
-        Point point2 = new Point(1,3);
-        Point point3 = new Point(5,8);
-        System.out.println(point1);
-        System.out.println(point2);
-        System.out.println(point3);
-        System.out.println(point1==point2);
-        System.out.println(point1==point3);
+
+        Line line1 = new Line(new Point(1,3),new Point(5,8));
+        Line line2 = new Line(new Point(10,11),new Point(15,19));
+        Line line3 = new Line(line1.coordinateEnd,line2.coordinateEnd);
+
+        System.out.println("Линия 3 до изменения: " + line3);
+
+        line3 = new Line(line1.coordinateEnd = new Point(6,9),line2.coordinateEnd=new Point(16,20));
+
+        System.out.println("Линия 3 после изменения: " + line3);
+
+        System.out.println("Суммарная длина всех трех линий = " + (line1.getLength()+line2.getLength()+line3.getLength()));
     }
 }
