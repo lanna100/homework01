13337afe12796984ebbb9682cd25118f15a777fd
Anna <m.a.s.1126@yandex.ru>
2024-09-10 01:33:50 +0300

    ООП Полиморфизм #2. Замкнутая ломаная

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -8,21 +8,16 @@ public class Main {
         Point[] points = {new Point(1, 5), new Point(2, 8),
                 new Point(5, 3), new Point(8, 9)};
         PolyLine polyLine = new PolyLine(points);
+        System.out.println("---------- Ломаная линия ----------");
         System.out.println(polyLine);
         System.out.println("Длина ломаной линии = " + polyLine.getLength());
         System.out.println(Arrays.toString(polyLine.getLines()));
 
-        Line[] lines = polyLine.getLines();
-        System.out.println("Длина массива Линий = " + lines.length);
-        double sumLinesLength = 0;
-        for (Line line : lines) {
-            sumLinesLength += line.getLength();
-        }
-        System.out.println("Сумма длин линий из массива = " + sumLinesLength);
-        System.out.println(polyLine.getLength() == sumLinesLength);
-        points[1] = new Point(12, 8);
-        System.out.println("Получаем результаты после изменения точки:");
-        System.out.println(polyLine);
-        System.out.println(Arrays.toString(polyLine.getLines()));
+        System.out.println("----- Замкнутая ломаная линия -----");
+        ClosedPolyLine closedPolyLine = new ClosedPolyLine(points);
+        System.out.println(closedPolyLine);
+        System.out.println("Длина замкнутой ломаной линии = " + closedPolyLine.getLength());
+        System.out.println(Arrays.toString(closedPolyLine.getLines()));
+
     }
 }
--- src/main/java/org/example/PolyLine.java
+++ src/main/java/org/example/PolyLine.java
@@ -32,3 +32,30 @@ public class PolyLine {
         return polyLineLength;
     }
 }
+
+class ClosedPolyLine extends PolyLine{
+    public ClosedPolyLine(Point[] points) {
+        super(points);
+    }
+    public String toString() {
+        return super.toString() + " + последняя точка " +
+                points[0];
+    }
+
+    public double getLength() {
+        double closedPolyLineLength = super.getLength();
+        Line closingLine = new Line(points[points.length - 1], points[0]);
+        closedPolyLineLength += closingLine.getLength();
+        return closedPolyLineLength;
+    }
+
+    public Line[] getLines() {
+        Line[] lines = new Line[points.length];
+        for (int i = 0; i < points.length - 1; i++) {
+            lines[i] = new Line(points[i], points[i + 1]);
+        }
+        lines[points.length - 1] = new Line(points[points.length - 1], points[0]); //замыкающая линия
+        return lines;
+    }
+
+}
