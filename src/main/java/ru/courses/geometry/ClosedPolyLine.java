9baf142cdb1ac3bb9c8591a99e6d27eac3e1b800
Anna <m.a.s.1126@yandex.ru>
2024-09-17 01:30:06 +0300

    ООП Пакеты #3. Возведение в степень

--- src/main/java/ru/courses/geometry/ClosedPolyLine.java
+++ src/main/java/ru/courses/geometry/ClosedPolyLine.java
@@ -1,39 +1,8 @@
-package org.example;
+package ru.courses.geometry;
 
-import java.util.Arrays;
+import ru.courses.geometry.interfaces.Measurable;
 
-public class PolyLine implements Measurable {
-    Point[] points;
-
-    public PolyLine(Point[] points) {
-        this.points = points;
-    }
-
-    @Override
-    public String toString() {
-        return "Линия " +
-                Arrays.toString(points);
-    }
-
-    public Line[] getLines() {
-        Line[] lines = new Line[points.length - 1];
-        for (int i = 0; i < points.length - 1; i++) {
-            lines[i] = new Line(points[i], points[i + 1]);
-        }
-        return lines;
-    }
-
-    public double getLength() {
-        double polyLineLength = 0;
-        for (int i = 0; i < points.length - 1; i++) {
-            Line line = new Line(points[i], points[i + 1]);
-            polyLineLength += line.getLength();
-        }
-        return polyLineLength;
-    }
-}
-
-class ClosedPolyLine extends PolyLine implements Measurable{
+public class ClosedPolyLine extends PolyLine implements Measurable {
     public ClosedPolyLine(Point[] points) {
         super(points);
         /*if (points.length > 0 && !points[0].equals(points[points.length - 1])) {
