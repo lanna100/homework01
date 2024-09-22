994ed90119c14102a5398d1b1e9088e3cfb42f3a
Anna <m.a.s.1126@yandex.ru>
2024-09-22 08:05:14 +0300

    ООП Класс Object #3. Сравнение линий

--- src/main/java/ru/courses/geometry/Line.java
+++ src/main/java/ru/courses/geometry/Line.java
@@ -1,36 +1,55 @@
 package ru.courses.geometry;
 import ru.courses.geometry.interfaces.Measurable;
 
-public class Line implements Measurable {
-    Point coordinateOrigin;
+import java.util.Objects;
+
+public class Line implements Measurable, Cloneable {
+    Point coordinateStart;
     Point coordinateEnd;
 
-    public Line(Point coordinateOrigin, Point coordinateEnd) {
-        this.coordinateOrigin = coordinateOrigin;
+    public Line(Point coordinateStart, Point coordinateEnd) {
+        this.coordinateStart = coordinateStart;
         this.coordinateEnd = coordinateEnd;
     }
 
     public Line(int x1, int y1, int x2, int y2) {
-        this.coordinateOrigin = new Point(x1, y1);
+        this.coordinateStart = new Point(x1, y1);
         this.coordinateEnd = new Point(x2, y2);
     }
 
     @Override
     public String toString() {
-        return "Линия от {" + coordinateOrigin.coordinateX +
-                ";" + coordinateOrigin.coordinateY +
+        return "Линия от {" + coordinateStart.coordinateX +
+                ";" + coordinateStart.coordinateY +
                 "} до {" + coordinateEnd.coordinateX +
                 ";" + coordinateEnd.coordinateY +
                 "}";
     }
 
     public double getLength() {
-        int a = coordinateEnd.coordinateX - coordinateOrigin.coordinateX;
-        int b = coordinateEnd.coordinateY - coordinateOrigin.coordinateY;
+        int a = coordinateEnd.coordinateX - coordinateStart.coordinateX;
+        int b = coordinateEnd.coordinateY - coordinateStart.coordinateY;
         return Math.sqrt(a * a + b * b);
     }
-}
-/*
 
-Может вернуть длину Линии. Для расчёта длины следует использовать формулу расчёта гипотенузы: корень суммы квадратов катетов.
-Для извлечения корня можно использовать Math.sqrt.  */
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Line line = (Line) o;
+        //return coordinateOrigin.equals(line.coordinateOrigin) && coordinateEnd.equals(line.coordinateEnd);
+        return Objects.equals(coordinateStart, line.coordinateStart) && Objects.equals(coordinateEnd, line.coordinateEnd);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(coordinateStart, coordinateEnd);
+    }
+
+    public Object clone() throws CloneNotSupportedException {
+        Line clonedLine = (Line) super.clone();
+        clonedLine.coordinateStart = (Point) coordinateStart.clone();
+        clonedLine.coordinateEnd = (Point) coordinateEnd.clone();
+        return clonedLine;
+    }
+}
--- src/main/java/ru/courses/main/Main.java
+++ src/main/java/ru/courses/main/Main.java
@@ -1,18 +1,21 @@
 package ru.courses.main;
+
 import ru.courses.geometry.*;
 import ru.courses.geometry.interfaces.Measurable;
+
 import java.util.Arrays;
 
 
 public class Main {
     public static void main(String[] args) {
+        //точки
         Point point1 = new Point(1, 2);
         Point point2 = new Point(1, 2);
         Point point3 = new Point(2, 1);
-
+        //сравнение точек
         System.out.println("point1 equals point2: " + point1.equals(point2)); // true
         System.out.println("point1 equals point3: " + point1.equals(point3)); // false
-
+        //клонирование точки
         Point clonedPoint = null;
         try {
             clonedPoint = (Point) point1.clone();
@@ -20,8 +23,26 @@ public class Main {
         } catch (CloneNotSupportedException e) {
             e.printStackTrace();
         }
-
+        //сравнение с клонированной точной
         System.out.println("point1 equals clonedPoint: " + point1.equals(clonedPoint)); //true
+
+        //линии
+        Line line1 = new Line(point1, point3);
+        Line line2 = new Line(point1, point3);
+        Line line3 = new Line(new Point(5, 6), new Point(7, 8));
+        // Сравнение линий
+        System.out.println("line1 equals line2: " + line1.equals(line2)); // true
+        System.out.println("line1 equals line3: " + line1.equals(line3)); // false
+        // Клонирование линии
+        Line clonedLine = null;
+        try {
+            clonedLine = (Line) line1.clone();
+            System.out.println("Cloned line: " + clonedLine);
+        } catch (CloneNotSupportedException e) {
+            e.printStackTrace();
+        }
+        //сравнение с клонированной линией
+        System.out.println("line1 equals clonedLine: " + line1.equals(clonedLine)); //true
 
         /*Предыдущие задания:
         Point myGeometryPoint = new Point(2, 3);
