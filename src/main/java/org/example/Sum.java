6efbd95ce5420a6d96149756eeaeefe6cfd1e4ae
Anna <m.a.s.1126@yandex.ru>
2024-09-26 11:24:31 +0300

    ООП Исключения #2. Необрабатываемые исключения

--- src/main/java/org/example/Sum.java
+++ src/main/java/org/example/Sum.java
@@ -1,14 +1,23 @@
 package org.example;
+
 public class Sum {
     public static void main(String[] args) {
         double sum = 0;
         for (String arg : args) {
-            try {
+            if (isNumeric(arg)) {
                 sum += Double.parseDouble(arg);
-            } catch (NumberFormatException ex) {
-                System.out.println("Wrong value: " + arg );
+            } else {
+                System.out.println("Некорректное значение: " + arg + " заменено на 0.");
             }
         }
         System.out.println(sum);
     }
+
+    //  -? значит '-' может появляться 0 или 1 раз
+    //  \\d+ значит любоая цифра от 0 до 9 может появляться один или более раз
+    //  (\\.\\d+)? значит блок в скобках (точка и любоая цифра от 0 до 9 может появляться один или более раз) может появляться 0 или 1 раз
+    private static boolean isNumeric(String str) {
+        return str != null && str.matches("-?\\d+(\\.\\d+)?");
+    }
+
 }
